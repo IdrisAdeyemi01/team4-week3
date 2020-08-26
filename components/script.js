54cd9f7aa73925e160b6b019d6caa4317982af5a
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-08-26 20:43:00 +0100

    Update script.js

--- components/script.js
+++ components/script.js
@@ -1,8 +1,22 @@
-let textarea = document.querySelector('text-cont');
-function changeButtomBorder() {
+let txtCount = document.querySelector('#txt-count');
+let textarea = document.querySelector('#text-cont');
+textarea.value = textarea.value.replace(/^\s*|\s*$/g,'');
+
+function changeBottomBorder() {
+    textarea.style.borderColor = '#FDB600';
+    var count = textarea.value.split(" ").length;
+    txtCount.innerHTML = count + ' / 500 Words';
+    if(count == 500) {
+        setTimout(showAlert(), 5000);
+    }
     if (textarea.value.length == 0) {
       textarea.style.borderBottom = '4px solid #E4E4E4';
     } else {
       textarea.style.borderBottom = '4px solid #FDB600';
     }
+};
+
+function showAlert() {
+    let errorMsg = document.querySelector('#errorMsg');
+    errorMsg.style.display = "block";
 }
