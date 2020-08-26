9c43e4e227bf17da1c0193d1139d3b283874de9d
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-08-26 22:06:50 +0100

    Update script.js

--- components/script.js
+++ components/script.js
@@ -4,19 +4,35 @@ textarea.value = textarea.value.replace(/^\s*|\s*$/g,'');
 
 function changeBottomBorder() {
     textarea.style.borderColor = '#FDB600';
-    var count = textarea.value.split(" ").length;
+    var count = (textarea.value.split(" ").length) - 1;
     txtCount.innerHTML = count + ' / 500 Words';
-    if(count == 500) {
-        setTimout(showAlert(), 5000);
+    if(count === 5) {
+        showAle=t();
+        setTimeout (function () {
+            errorMsg.style.display = "none";
+        }, 5000);
     }
     if (textarea.value.length == 0) {
       textarea.style.borderBottom = '4px solid #E4E4E4';
+      count = 0;
     } else {
       textarea.style.borderBottom = '4px solid #FDB600';
     }
 };
 
-function showAlert() {
     let errorMsg = document.querySelector('#errorMsg');
+    
+    errorMsg.style.width = "44%";
+    errorMsg.style.display = "none";
+    errorMsg.style.marginLeft = '8%';
+    errorMsg.style.background = '#FFD874';
+    errorMsg.style.padding = "1%";
+    errorMsg.style.fontSize = "2vw";   
+    errorMsg.style.color = 'rgba(0, 0, 0, 0.88)';
+    errorMsg.style.position = 'absolute';
+    errorMsg.style.borderRadius = '5px';
+    errorMsg.style.top = '27%';
+
+function showAlert() {
     errorMsg.style.display = "block";
 }
