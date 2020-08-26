961dd17401a6a09d119b68846b6c1d67d8527599
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-08-26 17:40:32 +0100

    Update script.js

--- components/script.js
+++ components/script.js
@@ -1,4 +1,8 @@
 let textarea = document.querySelector('text-cont');
-textarea.onkeyup = function {
-    this.style.border = '4px solid #FDB600';
+function changeButtomBorder() {
+    if (textarea.value.length == 0) {
+      textarea.style.borderBottom = '4px solid #E4E4E4';
+    } else {
+      textarea.style.borderBottom = '4px solid #FDB600';
+    }
 }
